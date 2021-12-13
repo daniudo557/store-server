5e4bcec16b39cb245ad5fda41cd8c1d5ae3ae8b6
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-13 01:46:00 -0300

    Create src folder

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ const cors = require("cors");
 
 const sequelize = require("./database");
 
-const products = require("./defaultProducts");
+const products = require("./mocks/productsMock");
+const user = require("./mocks/userMock");
 
 const Product = require("./app/models/Product");
 const User = require("./app/models/User");
 
 sequelize.sync({ force: true }).then(() => {
   Product.bulkCreate(products);
-  User.create({
-    userName: "admin",
-    fullName: "Admin",
-    password: "$2b$10$HsTr7B7wngjKqiTERFg5MeovT9tFxqrWg6KZHL.5Sxf.uai1Kb4k2",
-    email: "admin@admin.com",
-    role: 1,
-  });
+  User.create(user);
 });
 
 const app = express();
---
+++ src/mocks/userMock.js
@@ -0,0 +1,9 @@
+const user = {
+  userName: "admin",
+  fullName: "Admin",
+  password: "$2b$10$HsTr7B7wngjKqiTERFg5MeovT9tFxqrWg6KZHL.5Sxf.uai1Kb4k2",
+  email: "admin@admin.com",
+  role: 1,
+};
+
+module.exports = user;
